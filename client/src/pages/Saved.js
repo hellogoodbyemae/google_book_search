92932718c6ed6dba4b600be82afb182f0de2be00
Margaret Egner <hellogoodbyemae@gmail.com>
2020-08-29 13:24:34 -0400

    add all files based off class act 9 thru 11

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Saved from "../components/";
+
+class Saved extends Component {
+    render() {
+        return (
+            <Saved/>
+        )
+    }
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Search from "../components/";
+
+class Search extends Component {
+    render() {
+        return (
+            <Search/>
+        )
+    }
+}
+
+export default Search;
